bd0763ba530f2c6a684101987ad1e8632f46ed06
Moriceau Bastien <s190686@ic2s124-07.univ-lemans.fr>
2022-01-26 11:26:30 +0100

    la directive est KO

--- dashproject/dashfront/src/app/components/recherche/search-bar-directive.directive.ts
+++ dashproject/dashfront/src/app/components/recherche/search-bar-directive.directive.ts
@@ -5,10 +5,11 @@ import { Directive, ElementRef, HostListener, Input } from '@angular/core';
 })
 export class SearchBarDirectiveDirective {
 
-  constructor() { }
+  constructor(private el: ElementRef) { }
   
   @HostListener('clicked').onClick(){
   	//appel service avec barre de recherche
+  	this.value="ok";
   }
 
 }
