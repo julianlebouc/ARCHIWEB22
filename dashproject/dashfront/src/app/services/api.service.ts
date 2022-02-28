a60f1bdd51f727645c76be2621f5bdd237e847dd
YohannRED <yohann.delacroix@hotmail.fr>
2022-02-28 11:48:41 +0100

    Update api.service.ts

--- dashproject/dashfront/src/app/services/api.service.ts
+++ dashproject/dashfront/src/app/services/api.service.ts
@@ -8,12 +8,24 @@ import { HttpClient, HttpHeaders, HttpErrorResponse } from '@angular/common/http
 })
 export class ApiService {
 
-	REST_API: string = 'http://127.0.0.1:3080/Musiques';
   constructor(private httpClient: HttpClient) { }
-  
- GetMusiques(){
-	console.log(this.httpClient.get(`${this.REST_API}`));
-	return this.httpClient.get(`${this.REST_API}`);
+
+  GetMusiques(type_recherche: number){
+    let _url: string = "";
+    switch(type_recherche)
+    {
+      case 0: //GET BY TRACK
+        _url = './assets/get_track.json'; //Remplacer par l'url spotify : https://api.spotify.com/v1/tracks/id
+        break;
+      case 1: //ARTISTE
+        _url = './assets/get_artiste.json'; //Remplacer par l'url spotify : https://api.spotify.com/v1/artists/id
+        break;
+      case 2: //ALBUM
+        _url = './assets/get_album.json'; //Remplacer par l'url spotify : https://api.spotify.com/v1/albums/id
+        break;
+    }
+  	console.log(this.httpClient.get(`${_url}`));
+  	return this.httpClient.get(`${_url}`);
 	}
 }
 
