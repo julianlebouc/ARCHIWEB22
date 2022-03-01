8cc2096da3b17dd153b51f1dec39aa18e0ef7543
jlebouc35 <julian.lebouc@etudiant.univ-rennes1.fr>
2022-03-01 11:33:17 +0100

    connexion / recuperation access token spotify

--- dashproject/Back/serveur.js
+++ dashproject/Back/serveur.js
@@ -20,6 +20,48 @@ app.get("/Musiques", (req, res) => {
 	);
 });
 
+ //Connexion à spotify, récupération de l'access token
+var SpotifyWebApi = require('spotify-web-api-node');
+
+var clientId = 'd119537df82148ad80691b7349e6e31b',
+  clientSecret = '46a5d2b6d4b24c8b8a917f0b48346109';
+
+// Create the api object with the credentials
+var spotifyApi = new SpotifyWebApi({
+  clientId: clientId,
+  clientSecret: clientSecret,
+  redirectUri : 'http://localhost:4200/callback/',
+  scopes: [
+    "user-read-private",
+    "user-read-email",
+    "playlist-modify-public",
+    "playlist-read-private",
+    "playlist-modify-private",
+    "user-read-currently-playing",
+    "user-library-read",
+    "user-library-modify"
+  ]
+});
+
+
+// Retrieve an access token.
+
+spotifyApi.clientCredentialsGrant().then(
+	function(data) {
+	console.log('The access token expires in ' + data.body['expires_in']);
+	console.log('The access token is ' + data.body['access_token']);
+
+	 // Save the access token so that it's used in future calls
+	 spotifyApi.setAccessToken(data.body['access_token']);
+	 spotifyApi.setRefreshToken(data.body['refresh_token']);
+	 },
+	 function(err) {
+	   console.log('Something went wrong when retrieving an access token', err);
+	 }
+);
+
+
 app.listen(port, () => {
 	console.log('Server running on port 3080');
 });
+
--- dashproject/dashfront/src/app/components/recherche/recherche.component.ts
+++ dashproject/dashfront/src/app/components/recherche/recherche.component.ts
@@ -15,38 +15,6 @@ export class RechercheComponent{
 
   constructor(private apiService: ApiService) {}
 
- loginSpotify(): void{
-  	const getUrlParameter =(sParam: any) => {
-  		let sPageURL = window.location.search.substring(1),
-  		sURLVariables = sPageURL != undefined && sPageURL.length >0 ?
-  		sPageURL.split(`#`) : [],
-  		sParameterName,
-  		i;
-  		let split_str = window.location.href.length > 0 ?
-  		window.location.href.split(`#`) : [];
-  		sURLVariables = split_str != undefined && split_str.length >1 && split_str[1].length > 0 ? split_str[1].split(`&`) : [];
-  		for(i=0; i< sURLVariables.length; i++){
-  			sParameterName = sURLVariables[i].split(`=`);
-			console.log("sParameterName : " +sParameterName[0] + " sParam : "+sParam);
-  			if(sParameterName[0]===sParam){
-  				return sParameterName[1]===undefined ? true :
-  				decodeURIComponent(sParameterName[1]);
-  			} 
-		}
-		return false;
-	};
-	const accessToken = getUrlParameter(`access_token`);
-	let client_id = `d119537df82148ad80691b7349e6e31b`;
-	let redirect_uri = `http://localhost:4200/callback/`;
-	const redirect = `https://accounts.spotify.com/authorize?client_id=${client_id}&response_type=token&redirect_uri=${redirect_uri}`; 
-	if(accessToken ==null || accessToken=="" || accessToken==undefined){
-		//window.location.replace(redirect);
-	}
-	this.token=accessToken.toString();
-	this.isConnected =true;
-	console.log("Le token d'access est : " + this.token);
-	}
-
 	/*
 	  Fonction appellée lors du clic sur le bouton recherche
 	  Utilise le service API pour effectuer la bonne requête selon le choix renseigné
